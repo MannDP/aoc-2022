e76ced2a630cf5543fef8acd3fc0b456fc7a0dad
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-07-21 21:14:11 -0600

    Day 2, part 2

--- day2/main.cc
+++ day2/main.cc
@@ -2,29 +2,36 @@
 #include <unordered_map>
 using namespace std;
 
+// rock, A = 1
+// paper, B = 2
+// scissors, C = 3
+
 int main(const int argc, const char* const argv[]) {
+    unordered_map<char, int> outcomePoints {
+        {'X', 0}, // lose
+        {'Y', 3}, // draw
+        {'Z', 6}, // win
+    };
+
+    unordered_map<char, unordered_map<char, char>> moveMap {
+        {'A', {{'X', 'C'}, {'Y', 'A'}, {'Z', 'B'}}},
+        {'B', {{'X', 'A'}, {'Y', 'B'}, {'Z', 'C'}}},
+        {'C', {{'X', 'B'}, {'Y', 'C'}, {'Z', 'A'}}},
+    };
+
     unordered_map<char, int> movePoints {
-        {'X', 1}, // rock
-        {'Y', 2}, // paper
-        {'Z', 3} // scissors
+        {'A', 1}, {'B', 2}, {'C', 3}
     };
 
     int total = 0;
     char o, m;
     while(cin >> o) {
         cin >> m;
-        total += movePoints.at(m);
+        total += outcomePoints.at(m);
 
-        if (o - 'A' == m - 'X') {
-            // draw
-            total += 3;
-        } else if (
-            (o == 'A' && m == 'Y') ||
-            (o == 'B' && m == 'Z') ||
-            (o == 'C' && m == 'X')
-        ) {
-            total += 6;
-        }
+        // determine move based on opponent and outcome
+        char move = moveMap.at(o).at(m);
+        total += movePoints.at(move);
     }
 
     cout << total << endl;
