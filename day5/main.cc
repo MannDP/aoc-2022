147d4b2763db0a05e8b0114a5bf46a0c6ceca89e
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-07-21 23:54:18 -0600

    Day 5, part 1

---
+++ day5/main.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <list>
+using namespace std;
+
+int main(const int argc, const char* const argv[]) {
+    // process initial state
+    string line;
+    getline(cin, line);
+
+    const int numStacks = line.size() / 3;
+    list<char> state[numStacks];
+    for (size_t i = 0; i < numStacks; i++) {
+        state[i] = list<char>{};
+    }
+
+    do {
+        if (isdigit(line.at(1))) break;
+
+        for (int stack = 0; stack < numStacks; stack++) {
+            char letter = line[stack * 3 + stack + 1];
+            if (letter != ' ') {
+                state[stack].push_front(letter);
+            }
+        }
+    } while (getline(cin, line));
+    getline(cin, line);
+
+    // process moves
+    int amount, from, to;
+    string muck;
+    while(getline(cin, line)) {
+        istringstream iss{line};
+        iss >> muck >> amount >> muck >> from >> muck >> to;
+        from -= 1;
+        to -= 1;
+        while (amount > 0) {
+            char c = state[from].back();
+            state[from].pop_back();
+            state[to].push_back(c);
+            amount--;
+        }
+    }
+
+    // print result
+    for (size_t i = 0; i < numStacks; i++) {
+        cout << state[i].back();
+    }
+    cout << endl;
+
+    // // DEBUG
+    // for (int stack = 0; stack < numStacks; stack++) {
+    //     for (const auto& c : state[stack]) {
+    //         cout << c << endl;
+    //     }
+    //     cout << "=====" << endl;
+    // }
+}
