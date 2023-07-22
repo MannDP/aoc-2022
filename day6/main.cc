5217b0cce37bc5e2b203ae898bebb73b9a0302fc
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-07-22 00:32:09 -0600

    Day 6, part 2

--- day6/main.cc
+++ day6/main.cc
@@ -3,17 +3,18 @@
 using namespace std;
 
 int main() {
+    const int lenPrefix = 14;
     unordered_set<char> characters;
     string input;
     getline(cin, input);
     
-    for (size_t i = 0; i <= input.size() - 4; i++) {
-        for (size_t j = 0; j < 4; j++) {
+    for (size_t i = 0; i <= input.size() - lenPrefix; i++) {
+        for (size_t j = 0; j < lenPrefix; j++) {
             characters.insert(input[i + j]);
         }
 
-        if (characters.size() == 4) {
-            cout << i + 4 << endl;
+        if (characters.size() == lenPrefix) {
+            cout << i + lenPrefix << endl;
             break;
         }
         characters.clear();
