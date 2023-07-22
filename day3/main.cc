99dfaea79eba1f56495dd2e1ec538f8a3af33888
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-07-21 22:06:20 -0600

    Day 3, part 2

--- day3/main.cc
+++ day3/main.cc
@@ -2,25 +2,41 @@
 #include <unordered_set>
 using namespace std;
 
+unordered_set<char> intersection(const unordered_set<char>& a, const unordered_set<char>& b) {
+    unordered_set<char> result;
+    for (const auto& c: a) {
+        if (b.find(c) != b.end()) {
+            result.insert(c);
+        }
+    }
+
+    for (const auto& c : b) {
+        if (a.find(c) != a.end()) {
+            result.insert(c);
+        }
+    }
+    return result;
+}
+
 int main(const int argc, const char* const argv[]) {
     int sum = 0;
-    string line;
-    while(getline(cin, line)) {
-        unordered_set<char> set{line.begin(), line.begin() + line.size() / 2};
+    string line1, line2, line3;
+    while(getline(cin, line1) && getline(cin, line2) && getline(cin, line3)) {
+        unordered_set<char> set1{line1.begin(), line1.end()};
+        unordered_set<char> set2{line2.begin(), line2.end()};
+        unordered_set<char> set3{line3.begin(), line3.end()};
+        unordered_set<char> set = intersection(set1, intersection(set2, set3));
+        assert(set.size() == 1);
 
-        for (size_t i = line.size() / 2; i < line.size(); ++i) {
-            char common = line[i];
-            if (set.find(common) != set.end()) {
-                if (isupper(common)) {
-                    sum += common - 'A' + 26;
-                } else {
-                    sum += common - 'a';
-                }
-                sum += 1;
-                break;
+        for (const auto& c : set) {
+            if (isupper(c)) {
+                sum += c - 'A' + 26;
+            } else {
+                sum += c - 'a';
             }
+            sum += 1;
+            break;
         }
-        // add the points of the common letter
     }
     
     cout << sum << endl;
