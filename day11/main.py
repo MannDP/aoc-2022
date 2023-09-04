de9036fe006a72a60ae5396ed947b58042429186
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-09-03 18:51:39 -0700

    Finish day 11 solution

--- day11/main.py
+++ day11/main.py
@@ -1,6 +1,8 @@
 from collections import deque
+from tqdm import tqdm
 from enum import Enum
 from math import floor
+
 
 class Operation(Enum):
     ADD = "+"
@@ -12,7 +14,7 @@ class Monkey:
         self.items = deque()
         self.op = None
         self.op_operand = None
-        
+
         self.test_operand = None
         self.true_recipient = None
         self.false_recipient = None
@@ -20,8 +22,8 @@ class Monkey:
         self.inspect_count = 0
 
     def construct(self):
-        input() # throw away the monkey header line
-        
+        input()  # throw away the monkey header line
+
         # handle items
         starting_items = input()
         starting_items = starting_items[starting_items.find(":")+1:]
@@ -31,11 +33,14 @@ class Monkey:
         # operation
         operation = input().strip()
         op_type = operation[21]
-        if op_type == "+": self.op = Operation.ADD
-        elif op_type == '*': self.op = Operation.MULTIPLY
-        else: assert False
+        if op_type == "+":
+            self.op = Operation.ADD
+        elif op_type == '*':
+            self.op = Operation.MULTIPLY
+        else:
+            assert False
         operation = operation[22:]
-        self.op_operand = operation.strip() # this may be numerical, or may be 'old'
+        self.op_operand = operation.strip()  # this may be numerical, or may be 'old'
 
         # division test
         self.test_operand = int(input().strip()[19:])
@@ -51,14 +56,15 @@ class Monkey:
             assert prop is not None
         return self
 
-    def play(self, monkeys):
+    def play(self, monkeys, lcm):
         count = len(self.items)
         self.inspect_count += count
         for _ in range(count):
             old_worry = self.items.popleft()
-            operand = old_worry if self.op_operand == "old" else int(self.op_operand)
+            operand = old_worry if self.op_operand == "old" else int(
+                self.op_operand)
             new_worry = old_worry + operand if self.op == Operation.ADD else old_worry * operand
-            new_worry = floor(new_worry / 3)
+            new_worry = new_worry % lcm
 
             test_result = new_worry % self.test_operand == 0
             recipient = self.true_recipient if test_result else self.false_recipient
@@ -69,14 +75,23 @@ def main():
     monkeys = []
     while True:
         monkeys.append(Monkey().construct())
-        try: input()
-        except Exception: break
-    for _ in range(20):
+        try:
+            input()
+        except Exception:
+            break
+
+    # a % m === (a % km) % m
+    lcm = 1
+    for monkey in monkeys:
+        lcm *= monkey.test_operand
+
+    for _ in tqdm(range(10000)):
         for monkey in monkeys:
-            monkey.play(monkeys)
+            monkey.play(monkeys, lcm)
 
     maxs = [m.inspect_count for m in monkeys]
     maxs.sort()
     print(maxs[-1] * maxs[-2])
+
 
 main()
