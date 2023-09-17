ac2ce2e5b6d23d658ff19d2e2381ff8f855b1888
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-09-16 21:29:56 -0700

    Day 2, part 2

--- src/main/scala/com/manndp/solutions/D2.scala
+++ src/main/scala/com/manndp/solutions/D2.scala
@@ -1,13 +1,15 @@
 package com.manndp.solutions
 
+import scala.collection.immutable.Map
+
 object D2 extends Solution {
-  private val shapeScore: Map[String, Int] = Map(
+  private val shapeScore = Map(
     "X" -> 1,
     "Y" -> 2,
     "Z" -> 3
   )
 
-  private val outcomeScore: Map[String, Map[String, Int]] = Map(
+  private val outcomeScore = Map(
     "A" -> Map("X" -> 3, "Y" -> 6, "Z" -> 0),
     "B" -> Map("X" -> 0, "Y" -> 3, "Z" -> 6),
     "C" -> Map("X" -> 6, "Y" -> 0, "Z" -> 3)
@@ -22,5 +24,19 @@ object D2 extends Solution {
     }))
   }
 
-  override def solve2(input: Seq[String]): Result = ???
+  override def solve2(input: Seq[String]): Result = {
+    val moveMap = Map(
+      "A" -> Map("X" -> "Z", "Y" -> "X", "Z" -> "Y"),
+      "B" -> Map("X" -> "X", "Y" -> "Y", "Z" -> "Z"),
+      "C" -> Map("X" -> "Y", "Y" -> "Z", "Z" -> "X")
+    )
+
+    ScalarResult(input.foldLeft(0)((acc, line) => {
+      val parts = line.split(" ")
+      val move = moveMap(parts(0))(parts(1))
+      val moveScore = shapeScore(move)
+      val roundScore = outcomeScore(parts(0))(move)
+      acc + moveScore + roundScore
+    }))
+  }
 }
